40e0b97ec40b66c69f569200c39a45216d4153d2
lemon <1041707577@qq.com>
2022-05-03 11:03:45 +0800

    feat : ref

---
+++ pages/vue3/ref/baseUse.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        {{ num }}
+        {{ refNum }}
+        {{ obj.a }}
+        {{ refObj.a }}
+    </div>
+</template>
+
+<script setup lang="ts">
+
+let num = 1;
+let refNum = ref(num)
+
+let obj = { a: 1 }
+let refObj = ref(obj)
+
+num++
+refNum.value += 1
+
+obj.a++;
+refObj.value.a++;
+
+</script>
+
+<style scoped>
+</style>
---
+++ pages/vue3/ref/unref.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        {{ realA }}
+        {{ realB }}
+    </div>
+</template>
+
+<script setup lang="ts">
+import { Ref } from 'vue';
+
+const a = 1;
+let b = ref(a)
+
+function getRealValue(val: number | Ref<number>) {
+    return unref(val)
+}
+
+const realA = getRealValue(a)
+const realB = getRealValue(b)
+
+</script>
+
+<style scoped>
+</style>
