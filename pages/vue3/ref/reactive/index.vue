d3a877ec7004a2cf488d7527c775b3095cf4b553
lemon <1041707577@qq.com>
2022-05-04 22:08:33 +0800

    feat reactive

---
+++ pages/vue3/ref/reactive/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+    </div>
+</template>
+
+<script setup>
+const data = { foo: { bar: 1 } }
+const stateReactive = reactive(data)
+const stateRef = ref(data)
+
+console.log('stateReactive' , stateReactive);
+console.log('stateReactive.foo' , stateReactive.foo);
+console.log('stateReactive.foo.bar' , stateReactive.foo.bar);
+console.log('stateRef' , stateRef);
+console.log('stateRef.value' , stateRef.value);
+</script>
+
+<style lang="scss" scoped>
+</style>
