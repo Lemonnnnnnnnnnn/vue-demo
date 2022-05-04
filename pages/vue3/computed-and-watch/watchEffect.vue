253f148a7e6396f07f88a16dd096d8392db8c38a
lemon <1041707577@qq.com>
2022-05-04 15:47:57 +0800

    feat: ref

---
+++ pages/vue3/computed-and-watch/watchEffect.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        {{ foo }}
+    </div>
+</template>
+
+<script setup lang="ts">
+const foo = ref(1)
+watchEffect(() => {
+    console.log(foo.value); // 将foo添加进更新依赖项
+})
+
+setTimeout(() => {
+    foo.value++; // 再次执行依赖函数
+}, 1000)
+
+</script>
+
+<style scoped>
+</style>
---
+++ pages/vue3/ref/shallowRef.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        state.foo : {{ state.foo }}
+        <div>
+            <button @click="editStateBad">edit 【state.value.foo】</button>
+        </div>
+        <div>
+            <button @click="editStateGood">edit 【state.value】</button>
+        </div>
+    </div>
+</template>
+
+<script setup>
+const state = shallowRef({ foo: 1 })
+function editStateBad() {
+    state.value.foo = 2
+}
+
+function editStateGood() {
+    state.value = {
+        foo: 2
+    }
+}
+
+</script>
+
+<style scoped>
+</style>
