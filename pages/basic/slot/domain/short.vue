9fd77bc3e00bafa1e31f0c36fc599a84fc8d5afd
linyuchen <Lyc1688!!>
2022-05-10 17:12:36 +0800

    feat: slot简写

---
+++ pages/basic/slot/domain/short.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <number-list v-slot="{ num }">
+            <h1 v-if="num === 5">{{ num }}</h1>
+            <div v-else>{{ num }}</div>
+        </number-list>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+import numberList from './number-list.vue';
+
+
+</script>
+
+<style scoped>
+</style>
