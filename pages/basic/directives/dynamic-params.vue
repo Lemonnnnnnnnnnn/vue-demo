077b308fca35b4acc2c0e1ca0856a69674339b0b
lemon <1041707577@qq.com>
2022-05-10 23:47:42 +0800

    feat: slot缩写

---
+++ pages/basic/directives/dynamic-params.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        {{ num }}
+        <button @[eventName]="handleClick">add</button>
+    </div>
+</template>
+
+<script setup>
+
+const num = ref(0)
+const eventName = 'click'
+const handleClick = () => {
+    num.value += 1
+}
+
+</script>
+
+<style scoped>
+</style>
---
+++ pages/basic/slot/name/short.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <pageVue>
+            <template #header>
+                <div>Header</div>
+            </template>
+
+            <template #default>content</template>
+
+            <template #footer>
+                <div>footer</div>
+            </template>
+        </pageVue>
+    </div>
+
+</template>
+
+<script setup lang="ts">
+import pageVue from './page.vue';
+
+</script>
+
+<style scoped>
+</style>
