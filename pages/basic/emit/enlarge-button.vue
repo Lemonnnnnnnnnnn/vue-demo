aa9343ed6331b1da4dbf8c6651402d70914bd32d
linyuchen <Lyc1688!!>
2022-05-11 17:41:26 +0800

    feat: emit

---
+++ pages/basic/emit/enlarge-button.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <button @click="$emit('parentMethod')">enlarge button</button>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+</style>
---
+++ pages/basic/emit/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <div :style="{ fontSize : fontSize + 'px' }">title</div>
+        <enlarge-button @parentMethod="handleClick"></enlarge-button>
+    </div>
+</template>
+
+<script setup lang="ts">
+import enlargeButton from './enlarge-button.vue';
+
+const fontSize = ref(10)
+
+const handleClick = () => {
+    fontSize.value +=1
+}
+
+</script>
+
+<style scoped>
+</style>
