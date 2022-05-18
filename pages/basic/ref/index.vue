6ad3ead7657cc034352b1e7adda3e23621bb8f72
linyuchen <Lyc1688!!>
2022-05-18 15:57:15 +0800

    feat: ref

---
+++ pages/basic/ref/index.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <input type="text" ref="input">
+        <my-button ref="my-button"></my-button>
+        <button :ref="el => buttonRef = el" @click="print2">button</button> 
+    </div>
+</template>
+
+<script>
+import myButton from './my-button'
+
+export default {
+    components: {
+        myButton
+    },
+    data() {
+        return {
+            buttonRef: null
+        }
+    },
+    methods: {
+        focusInput() {
+            this.$refs.input.focus()
+        },
+        print() {
+            this.$refs['my-button'].$refs.button.click()
+        },
+        print2(){
+            console.log('world');
+        }
+    },
+    mounted() {
+        this.focusInput()
+        this.print()
+        this.$data.buttonRef.click()
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ pages/basic/ref/my-button.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <button ref="button" @click="handleClick">my button</button>
+    </div>
+</template>
+
+<script>
+export default {
+    methods:{
+        handleClick(){
+            console.log('hello');
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
