e74fd269cc4124ee9be42e088bbd4901a795b5d1
lemon <1041707577@qq.com>
2022-05-24 21:50:24 +0800

    feat: computed

---
+++ pages/basic/computed.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        {{num}}
+        {{double}}
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return {
+                num : 1,
+            }
+        },
+        computed:{
+            double(){
+                return this.num * 2
+            }
+            // double : ()=> this.num * 2 // 访问不到this指向
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
