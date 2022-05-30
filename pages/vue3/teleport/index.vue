2d2332dae8380944b9be0054e458780196d6b5d3
lemon <1041707577@qq.com>
2022-05-30 21:41:49 +0800

    feat: teleport

---
+++ pages/vue3/teleport/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="wrapper">
+        <h1>teleport</h1>
+         <modal-button ></modal-button>
+    </div>
+</template>
+
+<script setup lang="ts">
+import modalButton from './modal-button.vue'; 
+</script>
+
+<style scoped>
+
+.wrapper{
+    padding : 5rem;
+    position: relative;
+    border: 4px saddlebrown solid;
+}
+</style>
---
+++ pages/vue3/teleport/modal-button.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+        <button @click="handleClick">open modal</button>
+        <teleport to="body">
+            <div v-if="openModal" class="mask">
+            </div>
+            <div v-if="openModal" class="modal">
+                <div>
+                    modal content
+                </div>
+                <button @click="handleClick"> close modal</button>
+            </div>
+        </teleport>
+
+    </div>
+</template>
+
+<script setup lang="ts">
+const openModal = ref(false)
+
+const handleClick = () => {
+    openModal.value = !openModal.value
+}
+
+
+
+</script>
+
+<style scoped>
+.mask {
+    position: absolute;
+    z-index: 50;
+    background: #000;
+    opacity: 0.5;
+    left: 0;
+    top: 0;
+    width: 100vw;
+    height: 100vh;
+}
+
+.modal {
+    width: 20rem;
+    z-index: 100;
+    height: 15rem;
+    position: absolute;
+    background: #fff;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+}
+</style>
