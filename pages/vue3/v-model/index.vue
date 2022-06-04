cc15db0cf23521bdaf927a5cfe5127a44612b5a4
lemon <1041707577@qq.com>
2022-06-04 23:08:02 +0800

    feat: v-model

---
+++ pages/vue3/v-model/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <!-- <myComponentVue v-model:title="title" /> -->
+        <myComponentVue :title="title" @update:title="updateTitle" />
+    </div>
+</template>
+
+<script setup lang="ts">
+import myComponentVue from './my-component.vue';
+
+const title = ref('title')
+
+const updateTitle = (value :string) =>{
+    title.value = value
+}
+
+</script>
---
+++ pages/vue3/v-model/my-component.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <input :value="props.title" type="text" @input="inputValue">
+    </div>
+</template>
+
+<script setup lang="ts">
+
+const emit = defineEmits(['update:title'])
+
+const props = defineProps({
+    title : String
+})
+
+const inputValue = (e )=>{
+    emit('update:title' , e.target.value)
+}
+
+</script>
