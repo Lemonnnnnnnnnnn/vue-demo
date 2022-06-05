53b88493cdf0e7fae20bda145083fbaba59fbe6c
lemon <1041707577@qq.com>
2022-06-05 11:32:03 +0800

    feat: style

---
+++ pages/vue3/style-trait/v-bind.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <p class="text">text</p>
+    </div>
+</template>
+
+<script setup >
+const theme = {color :'red'}
+
+</script>
+
+<style scoped>
+.text{
+    color: v-bind('theme.color')
+}
+</style>
